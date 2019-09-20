e8eda40841c90053493eda189250e15974143a30
Luis García Castro <luis.garcia@von-ds.com>
2019-09-20 18:31:42 -0500

    Added Spring Boot dependency

--- src/main/java/ec/com/lfgarcia22/tenpinbowling/Main.java
+++ src/main/java/ec/com/lfgarcia22/tenpinbowling/Main.java
@@ -1,11 +1,27 @@
 package ec.com.lfgarcia22.tenpinbowling;
 
-public class Main {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-
-        System.out.println("Application running");
-
+        SpringApplication.run(Main.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Application Started...");
+        LOG.info("");
+        LOG.info("Ten-Pin Bowling");
+        LOG.info("By:\tLuis García Castro");
+        LOG.info("");
+        LOG.info("");
+    }
 }
